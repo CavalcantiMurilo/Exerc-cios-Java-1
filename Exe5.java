ba6060dc81e03adba42eb6efb1b637fad2bca633
Murilo Cavalcanti <106565777+CavalcantiMurilo@users.noreply.github.com>
2024-01-16 15:31:25 -0300

    Create Exe5.java

---
+++ Exe5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double numero;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        numero = sc.nextDouble();
+
+        if(numero > 0){
+            System.out.println("O número é positivo.");
+        }else if(numero < 0){
+            System.out.println("O número é negativo.");
+        }else{
+            System.out.println("O número é igual a zero.");
+        }
+
+
+        sc.close();
+
+    }
+}
