6fa6862132a53b3f1ea89d0c6a9ae762891367b8
Murilo Cavalcanti <106565777+CavalcantiMurilo@users.noreply.github.com>
2024-01-22 09:59:10 -0300

    Implementação de tratamentos

--- Exe2.java
+++ Exe2.java
@@ -4,16 +4,19 @@ import java.lang.String;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char resposta;
-        System.out.println("Você tem carteira de motorista? S/N");
-        resposta = sc.nextLine().charAt(0);
+        String resposta;
 
-        if (resposta == 'S'){
+        System.out.println("Você tem carteira de motorista? S/N");
+        resposta = sc.nextLine();
+
+        resposta = resposta.trim();
+
+        if (resposta.equals("S") || resposta.equals("s")){
             System.out.println("Você pode dirigir!");
-        }else if(resposta =='N'){
+        }else if(resposta.equals("N") || resposta.equals("n")){
             System.out.println("Você não pode dirigir!");
         }else{
-            System.out.println("Resposta inválida");
+            System.out.println("Resposta inválida! Digite 'S/s' ou 'N/n'");
         }
 
         sc.close();
